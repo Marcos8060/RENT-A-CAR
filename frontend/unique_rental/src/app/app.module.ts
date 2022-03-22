df3a852087d682142ebf67420f2bc50d0a1b221c
Marcos8060 <marcosgav80@gmail.com>
2022-03-22 14:19:07 +0300

    created routes to different pages

--- frontend/unique_rental/src/app/app.module.ts
+++ frontend/unique_rental/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { FleetComponent } from './fleet/fleet.component';
 
+
+const appRoutes: Routes = [
+  { path: '' , component : HeroComponent },
+  { path: 'fleet', component : FleetComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,8 @@ import { FleetComponent } from './fleet/fleet.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
