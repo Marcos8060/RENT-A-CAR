1d8743d2ddf3414556b3a53df8dd7467a2296c8f
Marcos8060 <marcosgav80@gmail.com>
2022-03-22 13:35:58 +0300

    created the fleet component

--- frontend/unique_rental/src/app/app.module.ts
+++ frontend/unique_rental/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
+import { FleetComponent } from './fleet/fleet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    FleetComponent
   ],
   imports: [
     BrowserModule,
