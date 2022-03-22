9986857c4dcd58975bc699448769afd719221011
Marcos8060 <marcosgav80@gmail.com>
2022-03-22 16:58:52 +0300

    created the profile page and added static data

--- frontend/unique_rental/src/app/app.module.ts
+++ frontend/unique_rental/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { FleetComponent } from './fleet/fleet.component';
 import { DetailsComponent } from './details/details.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const appRoutes: Routes = [
   { path: '' , component : HeroComponent },
   { path: 'fleet', component : FleetComponent},
-  { path: 'details', component: DetailsComponent}
+  { path: 'details', component: DetailsComponent},
+  { path: 'profile', component: ProfileComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
     FleetComponent,
-    DetailsComponent
+    DetailsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
