922cc724884a7122aa714caa734d2d692a9a7301
CosBett <cosmasbett9@gmail.com>
2022-03-23 11:15:18 +0300

    add carModeland user serializers

--- backend/carentApi/serializer.py
+++ backend/carentApi/serializer.py
@@ -6,3 +6,16 @@ class Profile_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['user', 'profile_picture', 'name', 'location', 'email']
+
+class Car_Model_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car_Model
+        fields = ['id', 'name', 'model', 'human_cpty','luggage_cpty','hourly_rate', 'description','year','image', 'image_interior','image_rear']
+
+class User_Serializer(serializers.ModelSerializer):
+    profile = Profile_Serializer(read_only=True)
+    carModel = Car_Model_Serializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ['id','username', 'profile', 'carModels']
+
