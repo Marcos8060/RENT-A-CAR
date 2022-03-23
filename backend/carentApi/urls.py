d621a6f010b70363550e7d87ef739d04ff9a332b
CosBett <cosmasbett9@gmail.com>
2022-03-23 11:31:49 +0300

    configure API urls settings

---
+++ backend/carentApi/urls.py
@@ -0,0 +1,17 @@
+from django import views
+from django.urls import path,include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('profile', views.Profile_viewSet)
+router.register('car_models', views.CarModel_viewSet)
+router.register('users', views.User_viewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
+
+
+]
