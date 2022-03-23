d716cc1b6b7e9fb8a75afefbbb8959699dacc9a7
CosBett <cosmasbett9@gmail.com>
2022-03-23 09:29:24 +0300

    write test for profile model

--- backend/carentApi/tests.py
+++ backend/carentApi/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='Prime', password='123qwerty')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
